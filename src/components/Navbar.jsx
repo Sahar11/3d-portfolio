2aacd10ee74c9b1be737d4e9890684e9ee18eb4c
Sahar <saharhalim151@gmail.com>
2023-12-06 17:41:41 -0500

    dit Navbar and App.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { styles } from '../styles';
 import { navLinks } from '../constants';
 import { logo, menu, close} from '../assets';
@@ -7,16 +7,19 @@ import { logo, menu, close} from '../assets';
 const Navbar = () => {
 
   const [active, setActive] = useState("");
+
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
-     <div className="items-center w-full flex justify-between max-w-7xl mx-auto">
-      <Link className="flex items-center gap-2" onClick={() => {setActive("");
-    window.scrollTo(0, 0);
-    }}>
-      <img src={logo} alt="logo" className="w-30 h-30 object-contain" />
-    </Link>
-     </div>
+    <header className="header">
+    <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
+     <p className="blue-gradient_text">SH</p>
+    </NavLink>
+    <nav className="flex text-lg gap-7 font-medium">
+      <NavLink to="/about">
+        About
+      </NavLink>
     </nav>
+    </header>
+
   )
 }
 
