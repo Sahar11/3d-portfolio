bca7f1c3a322d36a8cf5084d3f856003db9c308b
Sahar <saharhalim151@gmail.com>
2023-12-06 17:43:33 -0500

    edit Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,8 +14,11 @@ const Navbar = () => {
      <p className="blue-gradient_text">SH</p>
     </NavLink>
     <nav className="flex text-lg gap-7 font-medium">
-      <NavLink to="/about">
+      <NavLink to="/about" className={({ isActive }) => isActive ? 'text-blue-500': 'text-black'}>
         About
+      </NavLink>
+      <NavLink to="/projects" className={({ isActive }) => isActive ? 'text-blue-500': 'text-black'}>
+        Projects
       </NavLink>
     </nav>
     </header>
