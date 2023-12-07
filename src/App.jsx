676944393485762a058a7d340874a9b725be6072
Sahar <saharhalim151@gmail.com>
2023-12-07 14:21:27 -0500

    create Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Projects  from './pages/Projects';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+
+
 
 const App = () => {
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import { styles } from '../styles';
-import { navLinks } from '../constants';
-import { logo, menu, close} from '../assets';
+import { NavLink } from "react-router-dom";
+import { logo } from "../assets";
+
+
 
 const Navbar = () => {
-
-
   return (
-    <header className="header">
-    <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
-     <p className="blue-gradient_text">SH</p>
-    </NavLink>
-    <nav className="flex text-lg gap-7 font-medium">
-      <NavLink to="/about" className={({ isActive }) => isActive ? 'text-blue-500': 'text-black'}>
-        About
+    <header className='header'>
+      <NavLink to='/'>
+        <img src={logo} alt='logo' className='w-20 h-20 object-contain' />
       </NavLink>
-      <NavLink to="/projects" className={({ isActive }) => isActive ? 'text-blue-500': 'text-black'}>
-        Projects
-      </NavLink>
-    </nav>
+      <nav className='flex text-lg gap-7 font-medium'>
+        <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
+          About
+        </NavLink>
+        <NavLink to='/projects' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"}>
+          Projects
+        </NavLink>
+      </nav>
     </header>
+  );
+};
 
-  )
-}
-
-export default Navbar
+export default Navbar;
