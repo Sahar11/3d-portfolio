c294c8e8c485d34b7607e988e5a79696f8131ac9
Sahar <saharhalim151@gmail.com>
2023-12-20 16:52:25 -0500

    adjust light and position

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,57 @@
-import { Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
-import Loader from '../components/Loader';
-import Island from '../models/island';
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import Loader from "../components/Loader";
+import Island from "../models/island";
 
-
-
-
- {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+{
+  /* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
         POPUP
-      </div> */}
-
-const Home = () => {
- const adjustIslandForScreenSize = () => {
-  let screenScale = null
-  let screenPosition =[0, -6.5, -43];
-
-  if(window.innerWidth < 768) {
-    screenscale = [0.9, 0.9, 0.9];  
-  } else {
-    screenScale = [1, 1, 1];
-  }
-  }
- 
-  return (
-    <section className="w-full h-screen relative">
-     <Canvas className="w-full h-screen bg-transparent" camera={ {near: 0.1, far: 1000}}>
-      <Suspense fallback={<Loader />}>
-        <directionalLight />
-        <ambientLight />
-        <pointLight />
-        <spotLight />
-        <hemisphereLight />
-        <Island />
-      </Suspense>
-     </Canvas>
-    </section>
-  )
+      </div> */
 }
 
+const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
 
-export default Home
+    if (window.innerWidth < 768) {
+      screenscale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition, rotation];
+  };
+
+  const [islandScale, islandPosition, islandRotation] =
+    adjustIslandForScreenSize();
+  return (
+    <section className="w-full h-screen relative">
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight position={[1, 1, 1]} intensity={3} />
+          <ambientLight intensity={0.5} />
+          <pointLight  position={[10, 5, 10]} intensity={2} />
+          <spotLight 
+          position={[0, 50, 10]}
+          angle={0.15}
+          penumbra={1}
+          intensity={2}
+           />
+          <hemisphereLight />
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+          />
+        </Suspense>
+      </Canvas>
+    </section>
+  );
+};
+
+export default Home;
